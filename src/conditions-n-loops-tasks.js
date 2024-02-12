c11cfaef64ae98ea97766ea09731134e28e8c164
vwital <wital128@tut.by>
2024-02-13 00:00:35 +0300

    feat solve sortByAsk

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -414,9 +414,23 @@ function rotateMatrix() {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const array = arr;
+  for (let i = 0; i < arr.length; i += 1) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j += 1) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    const buff = arr[i];
+    array[i] = array[min];
+    array[min] = buff;
+  }
+  return array;
 }
+
+console.log(sortByAsc([-2, 9, 5, -3]));
 
 /**
  * Shuffles characters in a string so that the characters with an odd index are moved to the end of the string at each iteration.
